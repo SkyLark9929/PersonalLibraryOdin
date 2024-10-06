44d43c447754e6f6bfe3ea3b46e59b6995f6ad15
SkySlark9929 <skyslark9929@gmail.com>
2024-10-06 12:46:31 +0300

    Add function for adding books from dialog
    The personal library needed some way to add books
    into it. I added funcitons to add books from recen
    tly created dialog.

--- source.js
+++ source.js
@@ -1,22 +1,54 @@
 const myLibrary = [];
+const placeholderBook = new Book('F.M. Dostoevskiy', 'the Possessed', '2421', 'INPROGRESS', '243');
+
+myLibrary.push(placeholderBook);
+
 let bookContainer, bookHeader, bookControlsContainer, bookEditButton, bookEditIcon,
-bookDeleteButton, bookDeleteIcon, bookPageNumber, bookStatus, bookStatusSpan, bookProgress;
+bookDeleteButton, bookDeleteIcon, bookPageNumber, bookStatusP, bookStatusSpan, bookProgress;
+let everyBookContainer;
 
 const ADD_BOOK_DIALOG = document.querySelector('.add-book-dialog');
 const ADD_BOOK_BTN = document.querySelector('.add-book');
 ADD_BOOK_BTN.addEventListener('click', displayAddBookDialog);
+const CONFIRM_ADD_BOOK_BTN = document.querySelector('.confirm-add-book');
+CONFIRM_ADD_BOOK_BTN.addEventListener('click', addBookToLibrary);
 const MAIN = document.querySelector('main');
 
-function Book(author, title, numberOfPages, status) {
+function Book(author, title, numberOfPages, status, completedPages) {
   this.author = author;
   this.title = title;
   this.numberOfPages = numberOfPages;
   this.status = status;
+  this.completedPages = completedPages;
+
+  this.progress = Math.round((completedPages / numberOfPages) * 100);
 };
 
 // Add book button and funcitons
-function addBookToLibrary(book, library) {
-  library.push(book);
+/// necessary book characteristics
+let book, author, title, bookStatus, numberOfPages, completedPages;
+
+/// necessary elements
+let bookTitleField = document.querySelector('#title');
+let bookAuthorField = document.querySelector('#author');
+let bookNumberOfPagesField = document.querySelector('#number-of-pages');
+let bookStatusField = document.querySelector('#status');
+let bookCompletedPagesField = document.querySelector('#completed-pages');
+
+/// add book function itself
+function addBookToLibrary() {
+  author = bookAuthorField.value;
+  title = bookTitleField.value;
+  numberOfPages = bookNumberOfPagesField.value;
+  bookStatus = bookStatusField.value;
+  completedPages = bookCompletedPagesField.value;
+
+  // add book to the library
+  book = new Book(author, title, numberOfPages, bookStatus, completedPages);
+  myLibrary.push(book);
+
+  // add book to the page
+  displayBooks(myLibrary);
 };
 
 function displayAddBookDialog(){
@@ -24,7 +56,15 @@ function displayAddBookDialog(){
 };
 
 // Display books functions
+// FIXME: If I call this funciton every time I need to add book, I will have to purge and reload all the elements which is inefficient
+// It would have been much better if I separated displayBook function and display all books in the library function
 function displayBooks(library){
+    everyBookContainer = document.querySelectorAll('.book-container');
+
+    for (let container of everyBookContainer){
+      container.remove();
+    };
+
     for(let book of library){ // at which level is it possible to reuse the DOM elements?
       //book Container
       bookContainer = document.createElement('div');
@@ -62,32 +102,24 @@ function displayBooks(library){
       bookContainer.appendChild(bookPageNumber);
 
       // book status
-      bookStatus = document.createElement('p');
-      bookStatus.classList.add('status');
-      bookStatus.textContent = 'STATUS: ';
+      bookStatusP = document.createElement('p');
+      bookStatusP.classList.add('status');
+      bookStatusP.textContent = 'STATUS: ';
       bookStatusSpan = document.createElement('span');
       bookStatusSpan.classList.add(book['status'].toLowerCase());
       bookStatusSpan.textContent = book.status;
-      bookStatus.appendChild(bookStatusSpan);
-      bookContainer.appendChild(bookStatus);
+      bookStatusP.appendChild(bookStatusSpan);
+      bookContainer.appendChild(bookStatusP);
 
       // book progress !incomplete
       bookProgress = document.createElement('p');
       bookProgress.classList.add('progress');
-      bookProgress.textContent = 'PROGRESS: 100%';
+      bookProgress.textContent = `PROGRESS: ${book.progress}%`;
       bookContainer.appendChild(bookProgress);
 
       // display that monstrosity
       MAIN.appendChild(bookContainer);
     };
 };
-
-let book1 = new Book('J.R. Tolkien', 'the Hobbit', '457', 'UNREAD');
-let book2 = new Book('Homer', 'Odyssey', '1132', 'INPROGRESS');
-let book3 = new Book('Jack London', 'Call of the Wild', '300', 'FINISHED');
-
-addBookToLibrary(book1, myLibrary);
-addBookToLibrary(book2, myLibrary);
-addBookToLibrary(book3, myLibrary);
 
 displayBooks(myLibrary);
